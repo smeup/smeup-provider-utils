f5299aad53a995cc4eb406b452ebf926af2ebbec
monferdini-smeup <57705564+monferdini-smeup@users.noreply.github.com>
2020-02-06 17:35:48 +0100

    Create checkHTTP.sh
    Controllo sonde Minions tramite query salt

---
+++ gestione-minions/checkHTTP.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+if [[ $# -ne 1 ]] ; then
+  echo "Usage: $0 IDMINION" >&2
+  exit 1
+fi
+
+LOGFILE=/home/ec2-user/gestione-minions/log/checkHTTP/$1.log
+
+echo check http su minion $1 LOGFILE: $LOGFILE
+echo check http su minion $1 eseguito il `date '+%d/%m/%Y %H:%M:%S'` > $LOGFILE
+
+sudo salt $1 http.query http://127.0.0.1:8080/smeup-provider-fe/ProbesService/connectivity >> $LOGFILE.con
+echo Check Connectivity : 
+cat $LOGFILE.con | grep Status
+sudo salt $1 http.query http://127.0.0.1:8080/smeup-provider-fe/ProbesService/appserver >> $LOGFILE.app
+echo Check App : 
+cat $LOGFILE.app | grep Status
+sudo salt $1 http.query http://127.0.0.1:8080/smeup-provider-fe/ProbesService/folders >> $LOGFILE.fol
+echo Check Folders : 
+cat $LOGFILE.fol | grep Status
+sudo salt $1 http.query http://127.0.0.1:8080/smeup-provider-fe/ProbesService/queues >> $LOGFILE.que
+echo Check Queues : 
+cat $LOGFILE.que | grep Status
+
+cat $LOGFILE.con $LOGFILE.app $LOGFILE.fol $LOGFILE.que >> $LOGFILE
+
+rm $LOGFILE.con
+rm $LOGFILE.app
+rm $LOGFILE.fol
+rm $LOGFILE.que
+
+
